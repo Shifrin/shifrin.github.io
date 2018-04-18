bff61d2e342a84d738b05e9d87f2b6ee913e976b
Mohammed Shifreen <mshifreen@gmail.com>
2018-04-18 22:39:45 +0300

    Initial front-page commit.

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by shifr on 6/18/2017.
+ */
+
+jQuery(document).ready(function($) {
+    var $root = $('html, body');
+
+    $('body').on('click', 'a.learn-more', function() {
+        var target = $(this).attr('href');
+
+        $root.animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+
+        return false;
+    });
+});
